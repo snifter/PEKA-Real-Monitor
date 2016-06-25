62832b467c4251105ce81826f1fd6f02701b2465
Marek Podsiadły <marekpodsiadly@gmail.com>
2016-06-25 21:51:32 +0200

    BollardDataService created with method for downloadin data from api

---
+++ src/data-miner/services/bollardDataService.js
@@ -0,0 +1,54 @@
+var config = require('../config/global');
+var http = require('http');
+
+var BollardDataService = function() { };
+BollardDataService.prototype.fetchDataFromApi = function() {
+  var request = http.request(config.stopPointBollardsApiUrl, (response) => {
+    response.setEncoding('utf8');
+    var responseBody = '';
+    response.on('data', (chunk) => {
+      responseBody += chunk;    
+    });
+    response.on('end', () => {
+      var data = JSON.parse(responseBody);
+      for (var i = 0; i < data.features.length; i++) {
+        console.log(data.features[i].id, data.features[i].properties.stop_name);
+      }
+      /*
+      response format
+
+      {"features": [
+        {  
+           "geometry":{  
+              "coordinates":[  
+                 16.9343,
+                 52.39924
+              ],
+              "type":"Point"
+           },
+           "id":"AWF41",
+           "type":"Feature",
+           "properties":{  
+              "zone":"A",
+              "route_type":"0",
+              "headsigns":"6, 9, 12, 18, 20, 27",
+              "stop_name":"AWF"
+           }
+        },
+        ...
+        ],
+        "crs":{"type":"none","properties":{"info":"no information"}},
+        "type":"FeatureCollection"
+      }
+      */
+    })
+  });
+
+  request.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  request.end();
+}
+
+module.exports = BollardDataService;
