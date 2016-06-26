a84bc38be0c619eba014cea1c04f3850c696b159
Marek Podsiadły <marekpodsiadly@gmail.com>
2016-06-26 20:24:50 +0200

    BollardDataService.fetchDataFromApi returns a promise now

--- src/data-miner/main.js
+++ src/data-miner/main.js
@@ -1,5 +1,12 @@
 var BollardDataService = require('./services/bollardDataService');
 
 var bollardDataService = new BollardDataService();
-bollardDataService.fetchDataFromApi();
+var promise = bollardDataService.fetchDataFromApi();
 
+promise.then(function(data) {
+  for (var i = 0; i < data.features.length; i++) {
+    console.log(data.features[i].id, data.features[i].properties.stop_name);
+  }
+}).catch(function(e) {
+  console.error(`problem with request: ${e.message}`);
+});
