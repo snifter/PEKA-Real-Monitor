e2aa855db2e71f8f409b43858f036acb4cab2b44
Marek Podsiadły <marekpodsiadly@gmail.com>
2016-06-27 21:24:11 +0200

    MongoDB 3.2 added to dependencies

--- setup/installRuntimeDependencies.sh
+++ setup/installRuntimeDependencies.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# node.js 6.2
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# MongoDB 3.2
+# commands below install mongo on Ubuntu 14.04
+# https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
