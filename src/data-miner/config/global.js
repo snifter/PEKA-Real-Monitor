4bd81defa4f148914762a264c60cb499ec40d2cd
Marek Podsiadły <marekpodsiadly@gmail.com>
2016-06-30 21:00:10 +0200

    lines api endpoints configuration

--- src/data-miner/config/global.js
+++ src/data-miner/config/global.js
@@ -1,7 +1,13 @@
 module.exports = {
   services: {
     bollardDataService: {
-      stopPointBollardsApiUrl : 'http://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=cluster'      
+      stopPointBollardsApiUrl: "http://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=cluster"      
+    },
+    linesDataService: {
+      nightBusLinesApiUrl: "https://www.um.poznan.pl/ws/services/MPKLines/getBusLines?json=%7B%22type%22%3A%22n%22%7D",
+      dayBusLinesApiUrl: "https://www.um.poznan.pl/ws/services/MPKLines/getBusLines?json=%7B%22type%22%3A%22d%22%7D",
+      nightTramLinesApiUrl: "https://www.um.poznan.pl/ws/services/MPKLines/getTramLines?json=%7B%22type%22%3A%22n%22%7D",
+      dayTramLinesApiUrl: "https://www.um.poznan.pl/ws/services/MPKLines/getTramLines?json=%7B%22type%22%3A%22d%22%7D"
     }
   },
   databases: {
